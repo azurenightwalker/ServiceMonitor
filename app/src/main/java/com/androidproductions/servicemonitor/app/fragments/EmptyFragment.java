2974e7fbf677d9b1043544a32d021459a052c37b
Scott Jones <scott.jones@x-labsystems.co.uk>
2014-04-24 17:30:00 +0100

    Added GCM and client libraries to the app

--- app/src/main/java/com/androidproductions/servicemonitor/app/fragments/EmptyFragment.java
+++ app/src/main/java/com/androidproductions/servicemonitor/app/fragments/EmptyFragment.java
@@ -2,12 +2,21 @@ package com.androidproductions.servicemonitor.app.fragments;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.androidproductions.servicemonitor.app.R;
+import com.androidproductions.servicemonitor.backend.services.Services;
+import com.androidproductions.servicemonitor.backend.services.model.CollectionResponseServiceRecord;
+import com.androidproductions.servicemonitor.backend.services.model.ServiceRecord;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+
+import java.io.IOException;
 
 /**
  * A simple {@link android.app.Fragment} subclass.
@@ -37,7 +46,9 @@ public class EmptyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_empty, container, false);
+        View v = inflater.inflate(R.layout.fragment_empty, container, false);
+        getServices();
+        return v;
     }
 
     @Override
@@ -55,5 +66,28 @@ public class EmptyFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    private void getServices() {
+        new AsyncTask<Void, Void, CollectionResponseServiceRecord>() {
+            @Override
+            protected CollectionResponseServiceRecord doInBackground(Void... params) {
+                Services.Builder builder = new Services.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null);
+                try {
+                    return builder.build().listServices("test").execute();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(CollectionResponseServiceRecord msg) {
+                for (ServiceRecord srv : msg.getItems())
+                {
+                    Toast.makeText(getActivity(), srv.getServiceId() + srv.getStatus(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }.execute(null, null, null);
     }
 }
