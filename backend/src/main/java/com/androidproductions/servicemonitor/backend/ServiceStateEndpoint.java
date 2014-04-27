9828b7ec5e458d91351ddadd35315794d74464b6
Scott Jones <azurenightwalker@gmail.com>
2014-04-27 17:02:29 +0100

    API update. Initial auth work

--- backend/src/main/java/com/androidproductions/servicemonitor/backend/ServiceStateEndpoint.java
+++ backend/src/main/java/com/androidproductions/servicemonitor/backend/ServiceStateEndpoint.java
@@ -5,6 +5,8 @@ import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 import com.google.api.server.spi.response.CollectionResponse;
+import com.google.appengine.api.oauth.OAuthRequestException;
+import com.google.appengine.api.users.User;
 
 import java.io.IOException;
 import java.util.List;
@@ -14,7 +16,15 @@ import javax.inject.Named;
 
 import static com.androidproductions.servicemonitor.backend.OfyService.ofy;
 
-@Api(name = "services", version = "v1.2", namespace = @ApiNamespace(ownerDomain = "backend.servicemonitor.androidproductions.com", ownerName = "backend.servicemonitor.androidproductions.com", packagePath=""))
+@Api(name = "services",
+        version = "v1.3",
+        namespace = @ApiNamespace(
+                ownerDomain = "backend.servicemonitor.androidproductions.com",
+                ownerName = "backend.servicemonitor.androidproductions.com",
+                packagePath="")/*,
+        clientIds = {Constants.WEB_CLIENT_ID, Constants.ANDROID_CLIENT_ID, Constants.IOS_CLIENT_ID},
+        audiences = {Constants.ANDROID_AUDIENCE}*/
+)
 public class ServiceStateEndpoint {
 
     private static final Logger log = Logger.getLogger(ServiceStateEndpoint.class.getName());
@@ -24,51 +34,57 @@ public class ServiceStateEndpoint {
     /**
      * Register a service to the backend
      *
-     * @param serviceId The Service name to add
+     * @param rec The Service to add
      */
-    @ApiMethod(name = "register")
-    public void registerService(@Named("serviceId") String serviceId, @Named("serviceGroup") String serviceGroup) {
-        if(findRecord(serviceId, serviceGroup) != null) {
-            log.info("Service " + serviceId + " already registered, skipping register");
+    @ApiMethod(name = "register", path = "services/{serviceGroup}", httpMethod = "POST")
+    public void registerService(@Named("serviceGroup") String serviceGroup, ServiceRecord rec, User user) throws OAuthRequestException {
+        if (user == null)
+            throw new OAuthRequestException("User unauthorized");
+        if(findRecord(rec.getServiceId(), serviceGroup) != null) {
+            log.info("Service " + rec.getServiceId() + " already registered, skipping register");
             return;
         }
-        ServiceRecord record = new ServiceRecord();
-        record.setServiceId(serviceId);
-        record.setServiceGroup(serviceGroup);
-        record.setStatus(1);
-        ofy().save().entity(record).now();
+        ofy().save().entity(rec).now();
     }
 
     /**
      * Update a service in the backend
      *
      * @param serviceId The Service name to add
-     * @param status The new service status
+     * @param rec The new service record
      */
-    @ApiMethod(name = "update")
-    public void updateService(@Named("serviceId") String serviceId, @Named("serviceGroup") String serviceGroup, @Named("status") int status) {
+    @ApiMethod(name = "update", path = "services/{serviceGroup}/{serviceId}", httpMethod = "PUT")
+    public void updateService(@Named("serviceId") String serviceId, @Named("serviceGroup") String serviceGroup, ServiceRecord rec, User user) throws OAuthRequestException {
+        if (user == null)
+            throw new OAuthRequestException("User unauthorized");
+
         if(findRecord(serviceId,serviceGroup) == null) {
             log.info("Service " + serviceId + " not registered, skipping update");
             return;
         }
 
         ServiceRecord record = findRecord(serviceId, serviceGroup);
-        record.setStatus(status);
+        int oldStatus = record.getStatus();
+        record.setStatus(rec.getStatus());
         ofy().save().entity(record).now();
 
-        if (status >= AlertLevel)
+        if (record.getStatus() >= AlertLevel || (oldStatus >= AlertLevel && record.getStatus() < AlertLevel))
         {
-            Message.Builder builder = new Message.Builder();
-            builder.addData("serviceId", record.getServiceId());
-            builder.addData("serviceGroup", record.getServiceGroup());
-            builder.addData("status", String.valueOf(record.getStatus()));
-            builder.addData("time", String.valueOf(record.getLastUpdate()));
-            Message msg = builder.build();
-            try {
-                GcmHelper.SendMessage(msg);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            SendAlert(record);
+        }
+    }
+
+    private void SendAlert(ServiceRecord record) {
+        Message.Builder builder = new Message.Builder();
+        builder.addData("serviceId", record.getServiceId());
+        builder.addData("serviceGroup", record.getServiceGroup());
+        builder.addData("status", String.valueOf(record.getStatus()));
+        builder.addData("time", String.valueOf(record.getLastUpdate()));
+        Message msg = builder.build();
+        try {
+            GcmHelper.SendMessage(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -77,8 +93,11 @@ public class ServiceStateEndpoint {
      *
      * @param serviceId The service Id to remove
      */
-    @ApiMethod(name = "unregister")
-    public void unregisterService(@Named("serviceId") String serviceId, @Named("serviceGroup") String serviceGroup) {
+    @ApiMethod(name = "unregister", httpMethod = "DELETE", path = "services/{serviceGroup}/{serviceId}")
+    public void unregisterService(@Named("serviceGroup") String serviceGroup, @Named("serviceId") String serviceId, User user) throws OAuthRequestException {
+        if (user == null)
+            throw new OAuthRequestException("User unauthorized");
+
         ServiceRecord record = findRecord(serviceId,serviceGroup);
         if(record == null) {
             log.info("Service " + serviceId + " not registered, skipping unregister");
@@ -94,7 +113,10 @@ public class ServiceStateEndpoint {
      * @return a list of services
      */
     @ApiMethod(name = "listServices")
-    public CollectionResponse<ServiceRecord> listServices(@Named("serviceGroup") String serviceGroup) {
+    public CollectionResponse<ServiceRecord> listServices(@Named("serviceGroup") String serviceGroup, User user) throws OAuthRequestException {
+        if (user == null)
+            throw new OAuthRequestException("User unauthorized");
+
         List<ServiceRecord> records = ofy().transactionless().load().type(ServiceRecord.class)
                 .filter("serviceGroup", serviceGroup).list();
         return CollectionResponse.<ServiceRecord>builder().setItems(records).build();
